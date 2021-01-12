1e0fc0289242fdd4a664e8211d94ed81c6214426
Furkat Gofurov <furkat.gofurov@est.tech>
2021-01-12 14:43:01 +0200

    Changed vxlan configutation from p2p to multicast

--- setup_overlay_network.sh
+++ setup_overlay_network.sh
@@ -9,7 +9,8 @@ VXLAN_NAME_BAREMETAL=${VXLAN_NAME_BAREMETAL:-"vxlan10"}
 VXLAN_NAME_PROVISIONING=${VXLAN_NAME_PROVISIONING:-"vxlan20"}
 VXLAN_ID_BAREMETAL=${VXLAN_ID_BAREMETAL:-"10"}
 VXLAN_ID_PROVISIONING=${VXLAN_ID_PROVISIONING:-"20"}
-REMOTE_HOST_IP=${REMOTE_HOST_IP:-"10.201.10.50"}
+MULTICAST_IP_BAREMETAL=${MULTICAST_IP_BAREMETAL:-"239.1.1.1"}
+MULTICAST_IP_PROVISIONING=${MULTICAST_IP_PROVISIONING:-"239.1.1.2"}
 NETWORK_INTERFACE=${NETWORK_INTERFACE:-"ens3"}
 BRIDGE_NAME_BAREMETAL=${BRIDGE_NAME_BAREMETAL:-"baremetal"}
 BRIDGE_NAME_PROVISIONING=${BRIDGE_NAME_PROVISIONING:-"provisioning"}
@@ -17,6 +18,7 @@ MTU_SIZE=${MTU_SIZE:-"1450"}
 PORT_ID=${PORT_ID:-"4789"}
 
 # (Note) Below line outputs IP address of ens3 interface without CIDR notation(i.e 10.201.10.50).
+# in case we will need it.
 ip a | grep ens3 | cut -d " " -f6 | awk 'NR==2{print $1}' | cut -d "/" -f 1
 
 # Description:
@@ -30,26 +32,26 @@ ip a | grep ens3 | cut -d " " -f6 | awk 'NR==2{print $1}' | cut -d "/" -f 1
 function setup_overlay_network() {
     local VXLAN_NAME="$1"
     local VXLAN_ID="$2"
-    local REMOTE_IP="$3"
-    local PORT_ID="$4"
-    local NETWORK_INTERFACE="$5"
+    local MULTICAST_IP="$3"
+    local DESTINATION_PORT="$4"
+    local INTERFACE="$5"
     local BRIDGE_NAME="$6"
     local PROVISIONING_INTERFACE="$7"
-    local MTU_SIZE="$8"
+    local MTU="$8"
     
-    sudo ip link add "${VXLAN_NAME}" type vxlan id "${VXLAN_ID}" remote "${REMOTE_IP}" dstport "${PORT_ID}" dev "${NETWORK_INTERFACE}"
+    sudo ip link add "${VXLAN_NAME}" type vxlan id "${VXLAN_ID}" group "${MULTICAST_IP}" dstport "${DESTINATION_PORT}" dev "${INTERFACE}"
     sudo ip link set "${VXLAN_NAME}" master "${BRIDGE_NAME}"
     sudo ip link set "${VXLAN_NAME}" up
-    sudo ip link set dev "${PROVISIONING_INTERFACE}" mtu "${MTU_SIZE}"
+    sudo ip link set dev "${PROVISIONING_INTERFACE}" mtu "${MTU}"
 }
 
 # Delete kind cluster
 kind delete cluster
 
 # Setup baremetal overlay network
-setup_overlay_network "$VXLAN_NAME_BAREMETAL" "$VXLAN_ID_BAREMETAL"  "$REMOTE_HOST_IP" "$PORT_ID" \
+setup_overlay_network "$VXLAN_NAME_BAREMETAL" "$VXLAN_ID_BAREMETAL"  "$MULTICAST_IP_BAREMETAL" "$PORT_ID" \
 "$NETWORK_INTERFACE" "$BRIDGE_NAME_BAREMETAL" "$CLUSTER_PROVISIONING_INTERFACE" "$MTU_SIZE" 
 
 # Setup provisioning overlay network
-setup_overlay_network "$VXLAN_NAME_PROVISIONING" "$VXLAN_ID_PROVISIONING"  "$REMOTE_HOST_IP" "$PORT_ID" \
+setup_overlay_network "$VXLAN_NAME_PROVISIONING" "$VXLAN_ID_PROVISIONING"  "$MULTICAST_IP_PROVISIONING" "$PORT_ID" \
 "$NETWORK_INTERFACE" "$BRIDGE_NAME_PROVISIONING" "$CLUSTER_PROVISIONING_INTERFACE" "$MTU_SIZE"
