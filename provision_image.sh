e8fc3b90b098bdbcc36db5d10c58fff9f93c77db
kashifest <kashif.khan@est.tech>
2021-01-14 17:31:25 +0200

    Parameterize scalability functionalities

---
+++ provision_image.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -xe
+
+# shellcheck disable=SC1091
+source lib/common.sh
+
+# shellcheck disable=SC1091
+source lib/images.sh
+
+VM_ID_COUNTER=1
+
+# Calculate overall number of BMHs
+let number_of_bmh=$NUM_NODES*$NUM_VMS
+
+# Patch bmh crs yaml spec with corresponding IP to download image for node provisioning 
+for ((bmh_id = 0 ; bmh_id < $number_of_bmh ; bmh_id++)); do
+
+    if [ "$bmh_id" != "0" ] && [[ $bmh_id%6 -eq 0 ]]; then 
+        if [ "$VM_ID_COUNTER" == "1" ]; then
+            VM_ID_COUNTER=$((VM_ID_COUNTER + 2 ))
+        else
+            VM_ID_COUNTER=$((VM_ID_COUNTER + 1 ))
+        fi
+    fi
+kubectl patch bmh node-$bmh_id -n metal3 --type merge -p '{"spec":{"image":{"checksum":"http://172.22.0.'${VM_ID_COUNTER}'/images/'${IMAGE_RAW_NAME}'.md5sum", "checksumType":"md5", "format":"raw", "url":"http://172.22.0.'${VM_ID_COUNTER}'/images/'${IMAGE_RAW_NAME}'"}}}'
+done
+
