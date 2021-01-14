c01d35580f5018c85f8f0552f83f7aa78e77e680
kashifest <kashif.khan@est.tech>
2021-01-14 17:31:25 +0200

    Parameterize scalability functionalities

--- lib/common.sh
+++ lib/common.sh
@@ -28,7 +28,6 @@ export MARIADB_HOST_IP="127.0.0.1"
 # Set ironic scalability variables
 export VM_ID=${VM_ID:-1}
 export NUM_VMS=${NUM_VMS:-1}
-#export IRONIC_SCALABILITY_TEST=${IRONIC_SCALABILITY_TEST:-false}
 
 # Additional DNS
 ADDN_DNS=${ADDN_DNS:-}
